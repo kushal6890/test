5b16be2d93acc53958af0d7b101fa2b2b4a90c42
kushalrreddy <30915419+kushalrreddy@users.noreply.github.com>
2018-10-12 16:25:45 -0400

    edited test1.sh file

--- test1.sh
+++ test1.sh
@@ -2,11 +2,11 @@
 
 USERNAME=craftworksin
 SERVER=corpdmz.staples.com
-ARTIFACTORY_USERNAME=
-ARTIFACTORY_PASSWORD=
+ARTIFACTORY_USERNAME=redku001
+ARTIFACTORY_PASSWORD=Hanuman6890
 
 echo "copying from corpdmz.staples.com"
 cd /tmp && sftp ${USERNAME}@${SERVER}:/files/continuous-deploy-test/2018_10_08_cd_test.zip .
 echo "Uploading to artifactory"
-curl -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}  -X PUT \"http://zrepo.staples.com/artifactory/craftworks-artifacts-local/2018_10_08_cd_test.zip\" /tmp/2018_10_08_cd_test.zip"
+curl -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}  -X PUT \"http://zrepo.staples.com/artifactory/craftworks-artifacts-local/2018_10_08_cd_test.zip\" /tmp/2018_10_08_cd_test.zip
 
